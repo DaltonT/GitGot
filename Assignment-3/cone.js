31e95d9bd9636e3912c56a0119cd6de12a2483d0
DaltonT <dtechmanski@cogswell.edu>
2017-02-12 09:48:33 -0800

    Update cone.js

--- Assignment-3/cone.js
+++ Assignment-3/cone.js
@@ -13,7 +13,7 @@ function init() {
 
     gl.clearColor( 0.0, 1.0, 0.0, 1.0 );
     
-    cone = new Cone( n );
+    cone = new Cone();
 
     render();
 }
@@ -22,5 +22,33 @@ function render() {
     gl.clear( gl.COLOR_BUFFER_BIT );
     cone.render();
 }
+
+function Cone() {
+    this.count = 3,
+    this.positions = {
+        values : new Float32Array([ 
+         0.0, 0.0,  // Vertex 0
+         1.0, 0.0,  // Vertex 1
+         1.0, 1.0,  // Vertex 2
+         ]), numComponents : 2
+     };
+     this.colors = {
+      values : new Float32Array([ ... ]),       numComponets : 3   };
+                                 
+    this.positions.buffer = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, this.positions.buffer);
+    gl.bufferData(gl.ARRAY_BUFFER, this.positions.values, gl.STATIC_DRAW);
+    this.positions.attributeLoc = gl.getAttribLocation(this.program, “vPosition”);  gl.enableVertexAttribArray(this.positions.attributeLoc);
+                                
+    this.render = function () {
+        gl.bindBuffer(gl.ARRAY_BUFFER, this.positions.buffer);
+        gl.vertexAttribPointer(this.positions.attributeLoc,  this.positions.numComponents, gl.FLOAT, 0, 0);
+                                 
+        var start = 0;
+        var count = this.count;
+        gl.drawArrays(gl.TRIANGLE_STRIP, start, count);
+    };
+};
+
 
 window.onload = init;
